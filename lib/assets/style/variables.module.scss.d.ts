44ed1d54653f29461475c434dba23f50c4f63eb3
Shucei <1003823477@qq.com>
2023-07-28 16:33:51 +0800

    feat:scss变量定义

---
+++ lib/assets/style/variables.module.scss.d.ts
@@ -0,0 +1,17 @@
+export interface IGlobalScss {
+  IconSize: string;
+  IconColor: string;
+  playerWidth: string;
+  playerHeight: string;
+  controlBarHeight: string;
+  controlBarBackgroundColor: string;
+  controlBarTextColor: string;
+  playButtonSize: string;
+  playButtonColor: string;
+  progressBarHeight: string;
+  progressBarBackgroundColor: string;
+  progressBarFillColor: string;
+  timeDisplayColor: string;
+}
+export const variables: IGlobalScss;
+export default variables;
--- lib/videoPlay/index.vue
+++ lib/videoPlay/index.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="player">
+  <div class="player-wrap">
     <video src="https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4"></video>
   </div>
-  <SvgIcon icon="Bfullscreen" className=""></SvgIcon>
+
 </template>
 
 <script lang="ts" setup>
-import SvgIcon from "../components/SvgIcon.vue";
-
-
+// import SvgIcon from "../components/SvgIcon.vue";
+import { ref, onMounted, onUnmounted, onUpdated, watch, defineProps } from "vue";
+import { defaultProps } from "./defaultProps";
+const props = defineProps(defaultProps);
 </script>
 
 <script lang="ts">
@@ -17,5 +18,10 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+@import "../assets/style/index.scss";
 
+.player-wrap {
+  width: v-bind(width);
+  height: v-bind(height);
+}
 </style>
